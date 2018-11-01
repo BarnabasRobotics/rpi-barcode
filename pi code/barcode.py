6d87741a198b66ce20082c5772631fd1981b53eb
Nauj1 <juanmm1818@gmail.com>
2018-10-31 22:27:40 -0700

    Added a text Gui and a few more functions
    Changed the database structure to be Students - > code -> {name, grade}. Also added a find fucntion to find students based on the barcode.

--- pi code/barcode.py
+++ pi code/barcode.py
@@ -4,20 +4,30 @@ import sys
 from firebase import firebase
 import json
 
-firebase = firebase.FirebaseApplication('https://rpi-barcode.firebaseio.com/', None)
+
+
+firebase=firebase.FirebaseApplication('https://rpi-barcode.firebaseio.com/',None)
 
 def barcode_reader():
-    """Barcode code obtained from 'brechmos' 
+    """Barcode code obtained from 'brechmos'
     https://www.raspberrypi.org/forums/viewtopic.php?f=45&t=55100"""
-    hid = {4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm',
-           17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y',
-           29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ',
-           45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';', 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'}
+    hid = {4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g',
+11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm',
+           17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23:
+'t', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y',
+           29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35:
+'6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ',
+           45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';', 52:
+'\'', 53: '~', 54: ',', 55: '.', 56: '/'}
 
-    hid2 = {4: 'A', 5: 'B', 6: 'C', 7: 'D', 8: 'E', 9: 'F', 10: 'G', 11: 'H', 12: 'I', 13: 'J', 14: 'K', 15: 'L', 16: 'M',
-            17: 'N', 18: 'O', 19: 'P', 20: 'Q', 21: 'R', 22: 'S', 23: 'T', 24: 'U', 25: 'V', 26: 'W', 27: 'X', 28: 'Y',
-            29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ',
-            45: '_', 46: '+', 47: '{', 48: '}', 49: '|', 51: ':', 52: '"', 53: '~', 54: '<', 55: '>', 56: '?'}
+    hid2 = {4: 'A', 5: 'B', 6: 'C', 7: 'D', 8: 'E', 9: 'F', 10: 'G',
+11: 'H', 12: 'I', 13: 'J', 14: 'K', 15: 'L', 16: 'M',
+            17: 'N', 18: 'O', 19: 'P', 20: 'Q', 21: 'R', 22: 'S', 23:
+'T', 24: 'U', 25: 'V', 26: 'W', 27: 'X', 28: 'Y',
+            29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35:
+'^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ',
+            45: '_', 46: '+', 47: '{', 48: '}', 49: '|', 51: ':', 52:
+'"', 53: '~', 54: '<', 55: '>', 56: '?'}
 
     fp = open('/dev/hidraw0', 'rb')
 
@@ -66,24 +76,73 @@ def barcode_reader():
                         ss += hid[int(ord(c))]
     return ss
 
+def update_firebase(name,ss,grade):
+    data = {"Name":name, 'Grade':grade}
+    sent = json.dumps(data)
+    firebase.put('','Student/'+ss,data)
 
-def update_firebase(name,ss):
-    data = { "Code": ss,"Name": name}
-    firebase.post('/Students', data)
+def add_Student():
+    print("Enter student's name: ")
+    name = raw_input()
+    print("Enter student's Grade: ")
+    grade = raw_input()
+    print("Please scan barcode: ")
+    ss = barcode_reader()
+    buff = raw_input()
+    file.write("["+ name +":"+ ss +"]")
+    update_firebase(name,ss,grade)
+    del name
+    return
 
+def show_DataBase():
+    result = firebase.get('/Student',None)
+    print ''
+    print result
+
+def remove_Student():
+    print 'Removed'
+
+def update_Student():
+    print 'Updated'
+
+def find_Student():
+    print("Please scan barcode: ")
+    ss = barcode_reader()
+    buff = raw_input()
+    print ''
+    studentInfo = firebase.get(('/Student/'+ss),None)
+    print studentInfo
 
 if __name__ == '__main__':
-    file=open("barcodes.text","w")
-    try:
-        while True:
-		    name = raw_input("Enter students name: ")
-            print("Please scan barcode: ")
-            ss = barcode_reader()
-			
-			
-            file.write("[" + name + ":" + ss + "]")
-			update_firebase(name,ss)
-			
-    except KeyboardInterrupt:
-        pass
+    file=open("barcodes.text","a")
+    char = ''
+
+    while char != 'e':
+        print('----------'*3)
+        print('Menu')
+        print('(A)dd a student')
+        print('(G)et list of students')
+        print('(F)ind student')
+        print('(U)pdate student info')
+        print('(D)elete a student')
+        print('(E)nd Program')
+        print('----------'*3)
+        char = raw_input()
+        print('User has pressed: '+char)
+        if(char is 'a' or char is 'A'):
+            add_Student()
+        elif(char is 'g' or char is 'G'):
+            show_DataBase()
+        elif(char is 'f' or char is 'F'):
+            find_Student()
+        elif(char is 'u' or char is 'U'):
+            update_Student()
+        elif(char is 'd' or char is 'D'):
+            remove_Student()
+        elif(char is 'e' or char is 'E'):
+            char = 'e'
+            print 'Program perminated'
+        else:
+            print 'Invalid charater entered!'
+
     file.close()
